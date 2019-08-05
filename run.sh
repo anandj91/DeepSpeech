4aa9e3569a9453326feec2a24971aca2ea595842
jinliangwei <jinliangwei1@gmail.com>
2019-08-05 01:20:45 -0600

    Fixed size seq

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./DeepSpeech.py --train_files ~/anand/data/voice/clips/train.csv --dev_files ~/anand/data/voice/clips/dev.csv --test_files ~/anand/data/voice/clips/test.csv --train_batch_size=$1
+./DeepSpeech.py --train_files ~/anand/data/voice2/clips/train.csv --dev_files ~/anand/data/voice2/clips/dev.csv --test_files ~/anand/data/voice2/clips/test.csv --train_batch_size=$1
